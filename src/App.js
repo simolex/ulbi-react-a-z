f41bf18aae14133c558c7c591df4674e1d6fe7cb
simolex <simolex@yandex.ru>
2023-01-10 16:29:56 +0300

    create class and function Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+import ClassCounter from "./components/classCounter";
+import Counter from "./components/Counter";
 
 function App() {
-  return <div className="App"> Приложение работает</div>;
+  return (
+    <div className="App">
+      <Counter />
+      <ClassCounter />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Counter.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+  function increment() {
+    setCount(count + 1);
+  }
+  function decrement() {
+    setCount(count - 1);
+  }
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={decrement}>decrement</button>
+      <button onClick={increment}>increment</button>
+    </div>
+  );
+};
+
+export default Counter;
