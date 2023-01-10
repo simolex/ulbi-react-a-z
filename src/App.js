473a61a2601b6348e23d1cb54ea864c162aed196
simolex <simolex@yandex.ru>
2023-01-10 17:20:05 +0300

    render PostItem from Array

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import PostItem from "./components/PostItem";
 import "./styles/App.css";
 
 function App() {
+  const [posts, setPosts] = useState([
+    { id: 1, title: "JavaScript 1", body: "JavaScript - язык программирования" },
+    { id: 2, title: "JavaScript 2", body: "JavaScript - язык программирования" },
+    { id: 3, title: "JavaScript 3", body: "JavaScript - язык программирования" },
+  ]);
+
   return (
     <div className="App">
-      <PostItem post={{ id: 1, title: "JavaScript", body: "JavaScript - язык программирования" }} />
-      <PostItem post={{ id: 2, title: "JavaScript", body: "JavaScript - язык программирования" }} />
-      <PostItem post={{ id: 3, title: "JavaScript", body: "JavaScript - язык программирования" }} />
-      <PostItem post={{ id: 4, title: "JavaScript", body: "JavaScript - язык программирования" }} />
-      <PostItem post={{ id: 5, title: "JavaScript", body: "JavaScript - язык программирования" }} />
+      <h1 style={{ textAlign: "center" }}>Список постов</h1>
+      {posts.map((post) => (
+        <PostItem key={post.id} post={post} />
+      ))}
     </div>
   );
 }
