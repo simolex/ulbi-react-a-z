7978f8538628d02adffa7229b98e0493322811c2
simolex <simolex@yandex.ru>
2023-01-13 15:40:01 +0300

    fix: route table

--- src/router/index.js
+++ src/router/index.js
@@ -6,15 +6,15 @@ import { Navigate } from "react-router-dom";
 import Login from "../components/pages/Login";
 
 export const privateRoutes = [
-  { path: "about", element: <About /> },
-  { path: "posts/:id", element: <PostIdPage /> },
-  { path: "posts", element: <Posts /> },
+  { path: "/about", element: <About /> },
+  { path: "/posts/:id", element: <PostIdPage /> },
+  { path: "/posts", element: <Posts /> },
   { path: "*", element: <Error /> },
-  { path: "login", element: <Navigate to="/posts" replace /> },
-  { path: "", element: <Navigate to="posts" replace /> },
+  { path: "/login", element: <Navigate to="/posts" replace /> },
+  { path: "/", element: <Navigate to="/posts" replace /> },
 ];
 
 export const publicRoutes = [
-  { path: "login", element: <Login /> },
-  { path: "*", element: <Navigate to="login" replace /> },
+  { path: "/login", element: <Login /> },
+  { path: "*", element: <Navigate to="/login" replace /> },
 ];
